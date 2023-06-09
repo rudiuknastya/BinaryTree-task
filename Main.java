e6147005f4f1b559e1ba10c7a54253f1c7838b54
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2023-06-09 19:31:04 +0300

    Created instance of BinaryTree and checked how it works

--- Main.java
+++ Main.java
@@ -2,6 +2,15 @@ package binaryTreeTask;
 
 public class Main {
     public static void main(String[] args) {
-        
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.insert('h');
+        binaryTree.insert('a');
+        binaryTree.insert('k');
+        binaryTree.insert('m');
+        binaryTree.insert('c');
+        binaryTree.bFS();
+        System.out.println();
+        binaryTree.dFS();
+
     }
 }
